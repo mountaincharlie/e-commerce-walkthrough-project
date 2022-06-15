710fc3d83128f34774d6633b321e2c5668208313
Charlie <ciharland@hotmail.co.uk>
2022-06-15 18:59:45 +0000

    feat: register product and category tables in admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+# CHECK p4 project how you did these (classes?)
+admin.site.register(Product)
+admin.site.register(Category)
