630987c0206f12d662a7ef29457bc3af7ecd0bda
Charlie <ciharland@hotmail.co.uk>
2022-06-16 10:00:34 +0000

    feat: add str function to Product table class

--- products/models.py
+++ products/models.py
@@ -39,3 +39,6 @@ class Product(models.Model):
         max_digits=6, decimal_places=1, null=True, blank=True
         )
     image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
