d8cf15e7fa1b4e31f3b2ec4ef71e5ccd590f3d28
Charlie <ciharland@hotmail.co.uk>
2022-06-17 14:27:06 +0000

    feat: add view for product_details

--- products/views.py
+++ products/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
 def all_products(request):
+    """
+    view for rendering the products.html template with all the products
+    """
 
     products = Product.objects.all()
     # data to be rendered in the html file
@@ -11,3 +14,18 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_details(request, product_id):
+    """
+    view for rendering the product_details.html template with
+    all the product's details, using its id in the url
+    """
+
+    product = get_object_or_404(Product, pk=product_id)
+    # data to be rendered in the html file
+    context = {
+        'product': product
+    }
+
+    return render(request, 'products/product_details.html', context)
