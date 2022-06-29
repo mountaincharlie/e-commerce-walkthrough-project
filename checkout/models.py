117515936132046b158d990755cd685d6dfa1212
Charlie <ciharland@hotmail.co.uk>
2022-06-29 08:52:42 +0000

    feat: use django-countries to create a dropdown selection for country

--- checkout/models.py
+++ checkout/models.py
@@ -2,17 +2,19 @@ import uuid  # for generating the order numbers
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
+from django_countries.fields import CountryField
 from products.models import Product
 
 
 class Order(models.Model):
     """ The whole order in the users bag """
 
-    order_number = models.CharField(max_length=32, null=False, editable=False) # it will be auto generated and unique
+    # it will be auto generated and unique
+    order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address_1 = models.CharField(max_length=80, null=False, blank=False)
